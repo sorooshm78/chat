25f4dd27c43c98f0b01d12b087af59470712569d
Soroush mohammadi <sorooshm78@gmail.com>
2022-11-07 19:59:05 +0330

    add test user api view

---
+++ chat/tests/test_user_api_view.py
@@ -0,0 +1,30 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+from ..serializers import UserModelSerializer
+
+
+class TestUserApiView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse("list_user")
+        self.user1 = User.objects.create(username="user1", password="a/@1234567")
+        self.user2 = User.objects.create(username="user2", password="a/@1234567")
+        self.user3 = User.objects.create(username="user3", password="a/@1234567")
+
+    def test_get_users_without_Authentication(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_all_users(self):
+        # get API response
+        self.client.force_login(self.user1)
+        response = self.client.get(self.url)
+
+        # get data from db
+        users = User.objects.exclude(id=self.user1.id)
+        serializer = UserModelSerializer(users, many=True)
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, 200)
