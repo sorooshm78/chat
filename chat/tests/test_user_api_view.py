9f31b427692c20d1c85814aba7f3f5c3d3260d7c
Soroush mohammadi <sorooshm78@gmail.com>
2022-11-08 10:28:22 +0330

    use status code

--- chat/tests/test_user_api_view.py
+++ chat/tests/test_user_api_view.py
@@ -2,6 +2,8 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+from rest_framework import status
+
 from ..serializers import UserModelSerializer
 
 
@@ -13,11 +15,11 @@ class TestUserApiView(TestCase):
         self.user2 = User.objects.create(username="user2", password="a/@1234567")
         self.user3 = User.objects.create(username="user3", password="a/@1234567")
 
-    def test_get_users_without_Authentication(self):
+    def test_get_users_without_authentication_403_status(self):
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_get_all_users(self):
+    def test_get_all_users_200_status(self):
         # get API response
         self.client.force_login(self.user1)
         response = self.client.get(self.url)
@@ -27,4 +29,4 @@ class TestUserApiView(TestCase):
         serializer = UserModelSerializer(users, many=True)
 
         self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
